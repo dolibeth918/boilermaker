2a1d7028d056e4227a4c3939c3d557735f7a2dfe
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-05-31 11:53:53 -0400

    add redux store

---
+++ browser/reducers/index.js
@@ -0,0 +1,37 @@
+// not using combineReducers yet but should for modularity
+import { combineReducers } from 'redux';
+
+// EXAMPLE OF USING COMBINEREDUCERS BELOW
+
+// const allPuppiesReducer = function(state = [], action) {
+//   switch (action.type) {
+//     case RECIEVE_PUPPIES:
+//       return action.puppies;
+//     default:
+//       return state;
+//   }
+// };
+
+// const singlePuppyReducer = function(state = {}, action) {
+//   switch (action.type) {
+//     case SELECT_PUPPY:
+//       return action.puppy;
+//     default:
+//       return state;
+//   }
+// };
+
+// const rootReducer = combineReducers({
+//   allPuppies: allPuppiesReducer,
+//   singlePuppy: singlePuppyReducer
+// });
+
+// export default rootReducer;
+
+// for now, using a dummy reducer
+
+const reducer = (state = {}, action) => {
+  return state;
+};
+
+export default reducer;
