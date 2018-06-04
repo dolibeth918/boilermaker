9f1ef9e38aa401525223e4e70e55d30ebcccb41f
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-06-03 20:32:32 -0400

    add methods for salting and hashing passwords

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -18,30 +18,43 @@ const User = db.define('user', {
 
 //hooks
 User.beforeCreate(userInstance => {
-  userInstance.password = setSaltAndPassword(userInstance.password);
+  setSaltAndPassword(userInstance);
 });
 
 User.beforeUpdate(userInstance => {
-  userInstance.password = setSaltAndPassword(userInstance.password);
+  setSaltAndPassword(userInstance);
 });
 
 // instance methods
 User.prototype.correctPassword = function(candidatePassword) {
-  // should return true or false for if the entered password matches
+  return (
+    this.Model.encryptPassword(candidatePassword, this.salt) === this.password
+  );
+};
+
+User.prototype.sanitize = function() {
+  return _.omit(this.toJSON(), ['password', 'salt']);
 };
 
 // class methods
 User.generateSalt = function() {
-  // this should generate our random salt
+  return crypto.randomBytes(16).toString('base64');
 };
 
 User.encryptPassword = function(plainText, salt) {
-  // accepts a plain text password and a salt, and returns its hash
+  const hash = crypto.createHash('sha1');
+  hash.update(plainText);
+  hash.update(salt);
+  return hash.digest('hex');
 };
 
 function setSaltAndPassword(user) {
   // we need to salt and hash again when the user enters their password for the first time
   // and do it again whenever they change it
+  if (user.changed('password')) {
+    user.salt = User.generateSalt();
+    user.password = User.encryptPassword(user.password, user.salt);
+  }
 }
 
 module.exports = User;
