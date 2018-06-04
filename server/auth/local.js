5cff07b055e0ff7f7018309dbabbf0831541d371
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-06-03 21:34:56 -0400

    refactor auth route to use async await

--- server/auth/local.js
+++ server/auth/local.js
@@ -1,24 +1,25 @@
 const router = require('express').Router();
 const { User } = require('../db/models');
 
-router.post('/login', (req, res, next) => {
-  User.findOne({
+router.post('/login', async (req, res, next) => {
+  const user = await User.findOne({
     where: {
       email: req.body.email
     }
-  })
-    .then(user => {
-      if (!user) res.status(401).send('User not found');
-      else if (!user.hasMatchingPassword(req.body.password))
-        res.status(401).send('Incorrect password');
-      else {
-        req.login(user, err => {
-          if (err) next(err);
-          else res.json(user);
-        });
-      }
-    })
-    .catch(next);
+  });
+  try {
+    if (!user) res.status(401).send('User not found');
+    else if (!user.hasMatchingPassword(req.body.password))
+      res.status(401).send('Incorrect password');
+    else {
+      req.login(user, err => {
+        if (err) next(err);
+        else res.json(user);
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
